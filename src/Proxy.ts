12c37254d4c5d6fc367d6618cf7df9b2645a4802
Nils Ceberg <nils.ceberg@gmail.com>
2018-01-15 19:51:35 +0100

    add express server

--- src/Proxy.ts
+++ src/Proxy.ts
@@ -1,5 +1,18 @@
+import * as express from "express";
+
 export class Proxy {
+  private server: express.Express;
+
   constructor() {
     console.log("proski started");
+    this.server = express();
+
+    this.server.get("/:service", this.query.bind(this));
+    this.server.listen(8080, () => console.log("listening on *:8080"));
+  }
+
+  private query(req: express.Request, res: express.Response) {
+    const service = req.params.service;
+    res.end(`hello world from ${service}!`);
   }
 }
