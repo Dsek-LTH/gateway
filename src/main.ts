0ec26fccfbc0e62fea3dcfbb88c7e6c54575ef8f
Nils Ceberg <nils.ceberg@gmail.com>
2018-02-26 04:12:58 +0100

    register some test services

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ import { ZmqConsumer } from "./consumers/ZmqConsumer";
 const gateway = new Gateway();
 
 gateway.registerService("login", new ZmqService(1338));
+gateway.registerService("profile", new ZmqService(1339));
+gateway.registerService("message", new ZmqService(1340));
 
 gateway.registerConsumer("zmq", new ZmqConsumer("tcp://*:6000"));
-gateway.registerConsumer("http", new HttpConsumer(8080));
+gateway.registerConsumer("http", new HttpConsumer(8082));
